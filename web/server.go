d1f8ac53c9fd013a4ed1295fdbaa5a57d5ff0c38
yul <1103221645@qq.com>
2023-05-02 09:59:35 +0800

    Server

--- web/server.go
+++ web/server.go
@@ -1,4 +1,33 @@
 package web
 
-type Server struct {
+import (
+	"net"
+	"net/http"
+)
+
+// 小技巧，确保一定实现了Server接口
+var _ Server = &HTTPServer{}
+
+type Server interface {
+	http.Handler
+	Start(addr string) error
+}
+
+type HTTPServer struct {
+}
+
+// ServeHTTP 处理请求的入口
+func (h *HTTPServer) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *HTTPServer) Start(addr string) error {
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	// 这里可以加一些回调函数
+	return http.Serve(l, h)
 }
--- web/server_test.go
+++ web/server_test.go
@@ -1,7 +1,16 @@
 package web
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestServer(t *testing.T) {
+	var h Server = &HTTPServer{}
 
+	// 用法一：完全给http包
+	//http.ListenAndServe(":8081", h)
+	//http.ListenAndServeTLS(":8082", "", "", h)
+
+	// 自己手动启动
+	h.Start(":8083")
 }
