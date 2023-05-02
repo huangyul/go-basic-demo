7a0dc66f39d4a7c559cb46159a8b6f8273dbbc52
yul <1103221645@qq.com>
2023-05-02 10:21:52 +0800

    route

--- web/server.go
+++ web/server.go
@@ -8,9 +8,17 @@ import (
 // 小技巧，确保一定实现了Server接口
 var _ Server = &HTTPServer{}
 
+type HandleFunc func(ctx Context)
+
 type Server interface {
 	http.Handler
 	Start(addr string) error
+
+	// AddRoute 增加注册路由的方法
+	// method 是HTTP方法
+	// path 是路由
+	// handleFunc 处理方法
+	AddRoute(method string, path string, handleFunc HandleFunc)
 }
 
 type HTTPServer struct {
@@ -30,4 +38,16 @@ func (h *HTTPServer) Start(addr string) error {
 
 	// 这里可以加一些回调函数
 	return http.Serve(l, h)
+}
+
+func (h *HTTPServer) AddRoute(method string, path string, handleFunc HandleFunc) {
+	// 注册到路由树里面
+}
+
+func (h *HTTPServer) Get(path string, handleFunc HandleFunc) {
+	h.AddRoute(http.MethodGet, path, handleFunc)
+}
+
+func (h *HTTPServer) Post(path string, handleFunc HandleFunc) {
+	h.AddRoute(http.MethodPost, path, handleFunc)
 }
--- web/server_test.go
+++ web/server_test.go
@@ -1,12 +1,13 @@
 package web
 
 import (
+	"net/http"
 	"testing"
 )
 
 func TestServer(t *testing.T) {
-	var h Server = &HTTPServer{}
-
+	var h = &HTTPServer{}
+	h.AddRoute(http.MethodGet, "/user", func(ctx Context) {})
 	// 用法一：完全给http包
 	//http.ListenAndServe(":8081", h)
 	//http.ListenAndServeTLS(":8082", "", "", h)
